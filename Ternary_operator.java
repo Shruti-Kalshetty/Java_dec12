ae4f4aefdaf87702bfd98a2582a540ee7a2274d8
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-12 13:19:09 +0530

    Add files via upload

---
+++ Ternary_operator.java
@@ -0,0 +1,12 @@
+
+public class Ternary_operator {
+
+	public static void main(String[] args) {
+        int num = 15;
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number is: " + result); 
+
+
+	}
+
+}
