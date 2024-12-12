1ee3b0815558aab25127be10079fb560aed82d1f
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-12 13:18:40 +0530

    Add files via upload

---
+++ Assignment_operator.java
@@ -0,0 +1,24 @@
+
+public class Assignment_operator {
+
+	public static void main(String[] args) {
+		 int a = 10;
+	        int b = 5;
+
+	        int c = a + b; 
+	        System.out.println("c = " + c);
+
+	        c += 5; 
+	        System.out.println("c += 5: " + c);
+
+	        c *= 2;
+	        System.out.println("c *= 2: " + c);
+	        
+	        c/=5;
+	        System.out.println("c/=5: " + c);
+
+	        c %= 7; 
+	        System.out.println("c %= 7: " + c);
+	}
+
+}
